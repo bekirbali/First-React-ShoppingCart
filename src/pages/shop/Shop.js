7bd84b0b93c3cd6f3cf60ba9e72b91ecde2ed613
bekirbali <bfbali43@gmail.com>
2023-02-09 21:19:28 +0300

    assests

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,5 +1,12 @@
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>MyTech Shop</h1>
+      </div>
+      <div className="products"></div>
+    </div>
+  );
 };
 
 export default Shop;
